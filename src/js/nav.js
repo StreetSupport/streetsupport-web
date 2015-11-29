200e8e9131b58fdf8dbf105fc0cac485e1452fe0
Phil Lennon <phil@akuda.co.uk>
2015-11-29 16:01:23 +0000

    add nav styling & overlay

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,15 +1,18 @@
 // .js-nav-push
 // .js-nav-container
+// .js-nav-overlay
 // .is-active
 
 exports.open = function() {
   console.log('nav open');
   $('.js-nav-container').addClass('is-active');
   $('.js-nav-push').addClass('is-active');
+  $('.js-nav-overlay').addClass('is-active');
 };
 
 exports.close = function() {
   console.log('nav close');
   $('.js-nav-container').removeClass('is-active');
   $('.js-nav-push').removeClass('is-active');
+  $('.js-nav-overlay').removeClass('is-active');    
 };
