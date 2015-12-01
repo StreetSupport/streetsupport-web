4339648b156857ceb2548188a7bbe82f42c1eeb9
Phil Lennon <phil@iampjl.co.uk>
2015-12-01 19:17:10 +0000

    update to foley 0.0.3 (adds critical inline css)

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,10 +1,10 @@
 // Load global config and gulp
-var config    = require('../config.json');
-var argv      = require('yargs').argv;
-var gulp      = require('gulp');
-var plumber   = require('gulp-plumber');
-var debug     = require('gulp-debug');
-var gulpif    = require('gulp-if');
+var config  = require(__dirname + '/config/foley.json');
+var argv    = require('yargs').argv;
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var debug   = require('gulp-debug');
+var gulpif  = require('gulp-if');
 
 // Specific task modules
 var del = require('del');
--- tasks/webpack.js
+++ tasks/webpack.js
@@ -1,15 +1,15 @@
 // Load global config and gulp
-var config    = require('../config.json');
-var argv      = require('yargs').argv;
-var gulp      = require('gulp');
-var plumber   = require('gulp-plumber');
-var debug     = require('gulp-debug');
-var gulpif    = require('gulp-if');
+var config  = require(__dirname + '/config/foley.json');
+var argv    = require('yargs').argv;
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var debug   = require('gulp-debug');
+var gulpif  = require('gulp-if');
 
 // Specific task modules
-var gutil = require('gulp-util');
-var webpack = require('webpack');
-var webpackConfig = require('../webpack.config.js');
+var gutil         = require('gulp-util');
+var webpack       = require('webpack');
+var webpackConfig = require(__dirname + '/config/webpack.config.js');
 
 // JS task
 gulp.task('webpack', function (callback) {
