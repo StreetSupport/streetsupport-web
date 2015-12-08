cfe21de86b0a4cccc23c4b675f9275f93c7744ff
Vince Lee <vslee888@gmail.com>
2015-12-08 11:54:41 +0000

    removed semi-colons

--- src/js/page-find-help.js
+++ src/js/page-find-help.js
@@ -1,24 +1,23 @@
-var shared = require('./shared');
-var Holder = require('holderjs');
+var Holder = require('holderjs')
 
 require.ensure(['./get-category-list', 'hogan.js'], function(require) {
-	var getCategory = require('./get-category-list');
-	var Hogan = require('hogan.js');
+	var getCategory = require('./get-category-list')
+	var Hogan = require('hogan.js')
 
 	// Get API data using promise
 	var data = getCategory.data().then(function (result) {
 
 		// Append object name for Hogan
-		var theData = { categories : result };
+		var theData = { categories : result }
 
 		// Compile and render template
-		var theTemplate = document.getElementById('js-category-list-tpl').innerHTML;
-		var compile = Hogan.compile(theTemplate);
-		var theOutput = compile.render(theData);
+		var theTemplate = document.getElementById('js-category-list-tpl').innerHTML
+		var compile = Hogan.compile(theTemplate)
+		var theOutput = compile.render(theData)
 
-		document.getElementById('js-category-list-output').innerHTML=theOutput;
+		document.getElementById('js-category-list-output').innerHTML = theOutput
 
 		// Load placeholder images
-		Holder.run();
-  });
-});
+		Holder.run()
+  })
+})
