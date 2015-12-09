7cd53180c5de76d195e671c837c0b1d941726988
Phil Lennon <phil@iampjl.co.uk>
2015-12-09 12:52:29 +0000

    refactor to remove shared module

--- src/js/page-generic.js
+++ src/js/page-generic.js
@@ -1,1 +1,7 @@
-var shared = require('./shared');
+// Page modules
+var FastClick = require('fastclick')
+var nav = require('./nav.js') // eslint-disable-line
+var Holder = require('holderjs') // eslint-disable-line
+
+// FastClick
+FastClick.attach(document.body)
