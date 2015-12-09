57bcf0c5c50fa21be51929d27c8f2c08da4ddeee
Phil Lennon <phil@iampjl.co.uk>
2015-12-09 12:51:17 +0000

    update to standardjs standard

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,34 +1,38 @@
-var openElement = '.js-nav-open';
-var closeElement = '.js-nav-close';
-var overlayElement = '.js-nav-overlay';
-var activeClass = 'is-active';
-var el = document.querySelectorAll('.js-nav-container, .js-nav-push, .js-nav-overlay, html, body');
+var openElement = '.js-nav-open'
+var closeElement = '.js-nav-close'
+var overlayElement = '.js-nav-overlay'
+var activeClass = 'is-active'
+var el = document.querySelectorAll('.js-nav-container, .js-nav-push, .js-nav-overlay, html, body')
 
-document.querySelector(openElement).addEventListener('click', function(e) {
-  open();
-});
+document.querySelector(openElement).addEventListener('click', function (e) {
+  open()
+})
 
-document.querySelector(closeElement).addEventListener('click', function(e) {
-  close();
-});
+document.querySelector(closeElement).addEventListener('click', function (e) {
+  close()
+})
 
-document.querySelector(overlayElement).addEventListener('click', function(e) {
-  close();
-});
+document.querySelector(overlayElement).addEventListener('click', function (e) {
+  close()
+})
 
 var open = function () {
+  var i
+
   for (i = 0; i < el.length; ++i) {
-    el[i].classList.add(activeClass);
+    el[i].classList.add(activeClass)
   }
-};
+}
 
 var close = function () {
+  var i
+
   for (i = 0; i < el.length; ++i) {
-    el[i].classList.remove(activeClass);
+    el[i].classList.remove(activeClass)
   }
-};
+}
 
 module.exports = {
   open: open,
   close: close
-};
+}
