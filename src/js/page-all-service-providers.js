f6fc3d64c2f627e64264aa110668d59a97a4964a
Vince Lee <vslee888@gmail.com>
2015-12-09 15:19:31 +0000

    fixed all service providers page

--- src/js/page-all-service-providers.js
+++ src/js/page-all-service-providers.js
@@ -7,32 +7,25 @@ FastClick.attach(document.body)
 
 var _ = require('lodash')
 
-function getParameterByName(name) {
-	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]")
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-  results = regex.exec(location.search)
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "))
-}
-
 require.ensure(['./get-all-providers', 'hogan.js'], function(require) {
-	var getCategory = require('./get-all-providers')
-	var Hogan = require('hogan.js')
+  var apiRoutes = require('./api')
+  var getApiData = require('./get-api-data')
+  var Hogan = require('hogan.js')
 
-	// Get API data using promise
-	var data = getCategory.data().then(function (result) {
+  // Get API data using promise
+  getApiData.data(apiRoutes.serviceProviders).then(function (result) {
+    var sorted = _.sortBy(result, function(provider) {
+      return provider.name
+    })
 
-		var sorted = _.sortBy(result, function(provider) {
-			return provider.name
-		})
+    // Append object name for Hogan
+    var theData = { organisations : sorted }
 
-		// Append object name for Hogan
-		var theData = { organisations : sorted }
+    // Compile and render category template
+    var theCategoryTemplate = document.getElementById('js-category-result-tpl').innerHTML
+    var compileCategory = Hogan.compile(theCategoryTemplate)
+    var theCategoryOutput = compileCategory.render(theData)
 
-		// Compile and render category template
-		var theCategoryTemplate = document.getElementById('js-category-result-tpl').innerHTML
-		var compileCategory = Hogan.compile(theCategoryTemplate)
-		var theCategoryOutput = compileCategory.render(theData)
-
-		document.getElementById('js-category-result-output').innerHTML=theCategoryOutput
+    document.getElementById('js-category-result-output').innerHTML=theCategoryOutput
   })
 })
