994811c6c38d434f01ec63caddfe69f73861985d
Vince Lee <vslee888@gmail.com>
2015-12-09 16:06:12 +0000

    wired up new sub categorised page

--- src/js/api.js
+++ src/js/api.js
@@ -1,16 +1,18 @@
 var live = 'http://api.streetsupport.net'
 var dev = 'http://localhost:55881' // eslint-disable-line
 
-var domainRoot = live
+var domainRoot = dev
 
 var serviceProvidersUrl = '/v1/service-providers/'
 var serviceCategoriesUrl = '/v1/service-categories/'
 var categoryServiceProvidersUrl = '/v1/categorised-service-providers/show/'
+var categoryServiceProvidersWithSubsUrl = '/v2/categorised-service-providers/show/'
 var organisationUrl = '/v1/service-providers/show/'
 
 module.exports = {
   serviceProviders: domainRoot + serviceProvidersUrl,
   serviceCategories: domainRoot + serviceCategoriesUrl,
   categoryServiceProviders: domainRoot + categoryServiceProvidersUrl,
+  subCategoryServiceProviders: domainRoot + categoryServiceProvidersWithSubsUrl,
   organisation: domainRoot + organisationUrl
 }
