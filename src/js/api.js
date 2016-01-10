bf0068ad942f67b7bdbecfd13f14d7c27e6d4b70
Vince Lee <vslee888@gmail.com>
2016-01-10 19:09:31 +0000

    point to staging

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@ var dev = 'http://localhost:55881' // eslint-disable-line
 var staging = 'http://streetsupport-api-staging.apphb.com' // eslint-disable-line
 var live = 'http://api.streetsupport.net' // eslint-disable-line
 
-var domainRoot = live
+var domainRoot = staging
 
 var serviceProvidersUrl = '/v2/service-providers/'
 var serviceCategoriesUrl = '/v2/service-categories/'
