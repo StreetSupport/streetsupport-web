5441a1b68dd7f261eb689d1c27c31837bd7a367f
Vince Lee <vslee888@gmail.com>
2016-01-29 10:34:18 +0000

    point live to live

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@ var dev = 'http://localhost:55881' // eslint-disable-line
 var staging = 'http://streetsupport-api-staging.apphb.com' // eslint-disable-line
 var live = 'http://api.streetsupport.net' // eslint-disable-line
 
-var domainRoot = staging
+var domainRoot = live
 
 var serviceProvidersUrl = '/v2/service-providers/'
 var serviceCategoriesUrl = '/v2/service-categories/'
