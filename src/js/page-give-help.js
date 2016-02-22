7148cf3e412148ca1db2dd5988e3b9a707ac49de
Vince Lee <vslee888@gmail.com>
2016-02-22 17:52:56 +0000

    needs fiddling

--- src/js/page-give-help.js
+++ src/js/page-give-help.js
@@ -24,13 +24,19 @@ require.ensure(['./api', './get-api-data', './needs', './template-render', 'spin
   // Get API data using promise
   getApiData.data(apiRoutes.needs).then(function (result) {
     var needsFromApi = result.data
-    var needsToUse = needsFromApi.length < 1 ? staticNeeds : needsFromApi
-
+    var theData
+    console.log(needsFromApi)
+    if (needsFromApi.length > 0) {
+      theData = { 'needs': needsFromApi }
+    } else { 
+      theData = staticNeeds
+    }
+    console.log(theData)
     var callback = function () {
       loading.stop()
       //socialShare.init()
     }
 
-    templating.renderTemplate('js-need-list-tpl', needsToUse, 'js-need-list-output', callback)
+    templating.renderTemplate('js-need-list-tpl', theData, 'js-need-list-output', callback)
   })
 })
