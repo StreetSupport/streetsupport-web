ddc7c6a458c600674797b9c0e0a4ade4c2bd8e61
Phil Lennon <phil@frontendphil.com>
2016-03-03 18:04:24 +0000

    add success/error message

--- src/js/models/VolunteerModel.js
+++ src/js/models/VolunteerModel.js
@@ -1,9 +1,19 @@
+/* global location, history */
+
 var ko = require('knockout')
 require('knockout.validation') // No variable here is deliberate!
 
 var apiRoutes = require('../api')
 var postApi = require('../post-api-data')
 var Spinner = require('spin.js')
+
+var formId = 'js-form'
+var failId = 'js-fail'
+var successId = 'js-success'
+var theForm = document.getElementById(formId)
+var theFail = document.getElementById(failId)
+var theSuccess = document.getElementById(successId)
+var hideClass = 'hide'
 
 var VolunteerModel = function () {
   var self = this
@@ -48,84 +58,24 @@ var VolunteerModel = function () {
       postApi.post(apiRoutes.createVolunteerEnquiry, payload).then(function (result) {
         loading.stop()
         if (result.statusCode.toString().charAt(0) !== '2') {
-          alert('server error')
-          // showElement(form)
+          theForm.classList.add(hideClass)
+          theFail.classList.remove(hideClass)
         } else {
-          alert('server submitted')
-          //showElement(successMessage)
+          theForm.classList.add(hideClass)
+          theSuccess.classList.remove(hideClass)
         }
       })
-
     } else {
       self.errors.showAllMessages()
 
       // Jump to top of form
       var url = location.href
-      location.href = "#jsForm"
+      location.href = '#' + formId
       history.replaceState(null, null, url)
     }
   }
 
   self.errors = ko.validation.group(self)
-
-  /*
-  var pageElementIds = {
-    'spinner': 'spin',
-    'form': 'jsForm',
-    'successMessage': 'jsSuccessMessage'
-  }
-  // browser crap
-  var spin = document.getElementById(pageElementIds.spinner)
-  var form = document.getElementById(pageElementIds.form)
-  var successMessage = document.getElementById(pageElementIds.successMessage)
-
-  var hideElement = function (element) {
-    element.className += ' hidden'
-  }
-  var showElement = function (element) {
-    element.className = element.className.replace(/(?:^|\s)hidden(?!\S)/g, '')
-  }
-
-  self.errorMessages = ko.observableArray()
-
-  self.hasErrors = ko.computed(function () {
-    return self.errorMessages().length > 0
-  })
-
-  var loading
-
-  var submitForm = function (e) {
-    e.preventDefault()
-
-    loading = new Spinner().spin(spin)
-    hideElement(form)
-
-    var payload = {
-      'FirstName': document.getElementById('firstname').value,
-      'LastName': document.getElementById('lastname').value,
-      'Email': document.getElementById('email').value,
-      'Telephone': document.getElementById('telephone').value,
-      'Postcode': document.getElementById('postcode').value,
-      'SkillsAndExperienceDescription': document.getElementById('skillsAndExperience').value,
-      'AvailabilityDescription': document.getElementById('availability').value,
-      'ResourcesDescription': document.getElementById('resources').value
-    }
-
-    postApi.post(apiRoutes.createVolunteerEnquiry, payload)
-    .then(function (result) {
-      loading.stop()
-      if (result.statusCode.toString().charAt(0) !== '2') {
-        self.errorMessages(result.messages)
-        showElement(form)
-      } else {
-        showElement(successMessage)
-      }
-    })
-  }
-
-  form.onsubmit = submitForm
-
-  */
 }
 
 module.exports = VolunteerModel
