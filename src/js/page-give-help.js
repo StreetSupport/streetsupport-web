36922d804db9acd914269096ef2e276b8d8d90b7
vince (polyhat) <vslee888@gmail.com>
2016-03-04 13:45:33 +0000

    fix give help mailto link

--- src/js/page-give-help.js
+++ src/js/page-give-help.js
@@ -4,18 +4,17 @@ var nav = require('./nav.js')
 var socialShare = require('./social-share')
 var analytics = require('./analytics')
 
-
 nav.init()
 analytics.init()
 FastClick.attach(document.body)
 
 // Load and process data
-require.ensure(['./api', './get-api-data', './needs', './template-render', 'spin.js'], function (require) {
+require.ensure(['./api', './get-api-data', './needs', './template-render', 'spin.js', 'lodash'], function (require) {
   var apiRoutes = require('./api')
   var getApiData = require('./get-api-data')
   var templating = require('./template-render')
   var Spinner = require('spin.js')
-  var staticNeeds = require('./needs')
+  var _ = require('lodash')
 
   // Spinner
   var spin = document.getElementById('spin')
@@ -24,17 +23,15 @@ require.ensure(['./api', './get-api-data', './needs', './template-render', 'spin
   // Get API data using promise
   getApiData.data(apiRoutes.needs).then(function (result) {
     var needsFromApi = result.data
-    var theData
-    console.log(needsFromApi)
-    if (needsFromApi.length > 0) {
-      theData = { 'needs': needsFromApi }
-    } else { 
-      theData = staticNeeds
-    }
-    console.log(theData)
+    _.each(needsFromApi, function(need) {
+      console.log('hello!')
+      console.log(need)
+      need.link = "mailto:givehelp@streetsupport.net?subject=" + need.serviceProviderId + " need: " + need.description
+    })
+    var theData = { 'needs': needsFromApi }
     var callback = function () {
       loading.stop()
-      //socialShare.init()
+      socialShare.init()
     }
 
     templating.renderTemplate('js-need-list-tpl', theData, 'js-need-list-output', callback)
