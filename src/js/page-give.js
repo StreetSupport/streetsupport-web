cf9b096be69e7c8f5cbca76bcad94c9da4ccae14
Phil Lennon <phil@frontendphil.com>
2016-03-10 16:11:48 +0000

    added bricks/variable card heights

--- src/js/page-give.js
+++ src/js/page-give.js
@@ -6,6 +6,7 @@ var analytics = require('./analytics')
 var awesomplete = require('imports?this=>window!../../node_modules/awesomplete/awesomplete.js')
 import List from 'list.js'
 import Holder from 'holderjs'
+import Bricks from 'bricks.js'
 
 nav.init()
 analytics.init()
@@ -22,6 +23,12 @@ var options = {
 var values = [
   { type: 'people', organisation: 'Mad Dogs', description: 'We support young people to get into employment so they can support themselves and live independent lives. Help with getting a job would be invaluable.' },
   { type: 'time', organisation: 'Coffee4Craig', description: 'Test result.' },
+  { type: 'time', organisation: 'Coffee4Craig', description: 'Another Test result.' },
+  { type: 'people', organisation: 'Mad Dogs', description: 'We support young people to get into employment so they can support themselves and live independent lives. Help with getting a job would be invaluable.' },
+  { type: 'time', organisation: 'Coffee4Craig', description: 'Test result.' },
+  { type: 'time', organisation: 'Coffee4Craig', description: 'Another Test result.' },
+  { type: 'people', organisation: 'Mad Dogs', description: 'We support young people to get into employment so they can support themselves and live independent lives. Help with getting a job would be invaluable.' },
+  { type: 'time', organisation: 'Coffee4Craig', description: 'Test result.' },
   { type: 'time', organisation: 'Coffee4Craig', description: 'Another Test result.' }
 ]
 
@@ -29,6 +36,39 @@ var theList = new List('js-result', options, values)
 
 console.log(theList)
 
+const sizes = [
+  { columns: 1, gutter: 20 },                   // assumed to be mobile, because of the missing mq property
+  { mq: '360px', columns: 1, gutter: 20 },
+  { mq: '480px', columns: 2, gutter: 20 },
+  { mq: '600px', columns: 2, gutter: 20 },
+  { mq: '800px', columns: 3, gutter: 20 }
+]
+
+// create an instance
+
+const instance = Bricks({
+  container: '.list',
+  packed:    'data-packed',        // if not prefixed with 'data-', it will be added
+  sizes:     sizes
+})
+
+instance
+  .on('pack',   () => console.log('ALL grid items packed.'))
+  .on('update', () => console.log('NEW grid items packed.'))
+  .on('resize', size => console.log('The grid has be re-packed to accommodate a new BREAKPOINT.'))
+
+// start it up, when the DOM is ready
+// note that if images are in the grid, you may need to wait for document.readyState === 'complete'
+
+document.addEventListener('DOMContentLoaded', event => {
+  instance
+    .resize(true)     // bind resize handler
+    .pack()           // pack initial items
+})
+
+theList.on('sortStart', () =>
+  instance.pack()
+)
 
 /*
  Example need data
