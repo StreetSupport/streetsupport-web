391515b8364e425aad33fc294eb0cd10800c4204
Phil Lennon <phil@frontendphil.com>
2016-03-24 11:13:58 +0000

    wip changes

--- src/js/page-give.js
+++ src/js/page-give.js
@@ -1,32 +1,18 @@
+// Common modules
+import './common'
+
 // Page modules
-var FastClick = require('fastclick')
-var nav = require('./nav.js')
-var socialShare = require('./social-share')
-var analytics = require('./analytics')
-var awesomplete = require('imports?this=>window!../../node_modules/awesomplete/awesomplete.js')
+var awesomplete = require('imports?this=>window!../../node_modules/awesomplete/awesomplete.js') // eslint-disable-line
 import List from 'list.js'
-var ListFuzzySearch = require('imports?this=>window!../../node_modules/list.fuzzysearch.js/dist/list.fuzzysearch.js')
+// var ListFuzzySearch = require('imports?this=>window!../../node_modules/list.fuzzysearch.js/dist/list.fuzzysearch.js')
 import Holder from 'holderjs'
 import Bricks from 'bricks.js'
-
-nav.init()
-analytics.init()
-FastClick.attach(document.body)
 
 // Run Holder
 Holder.run({})
 
-console.log(ListFuzzySearch)
-
-var options = {
-  valueNames: [ 'type', 'organisation', 'description' ],
-  item: '<li><h3 class="h3 type"></h3><p class="organisation"></p><p class="description"></p></li>',
-  plugins: [
-    ListFuzzySearch()
-  ]
-}
-
-var values = [
+// Example data
+var data = [
   { type: 'people', organisation: 'Mad Dogs', description: 'We support young people to get into employment so they can support themselves and live independent lives. Help with getting a job would be invaluable.' },
   { type: 'time', organisation: 'Coffee4Craig', description: 'Test result.' },
   { type: 'time', organisation: 'Coffee4Craig', description: 'Another Test result.' },
@@ -38,49 +24,61 @@ var values = [
   { type: 'time', organisation: 'Coffee4Craig', description: 'Another Test result.' }
 ]
 
-var theList = new List('js-result', options, values)
+// List.js
+var options = {
+  valueNames: [ 'type', 'organisation', 'description' ],
+  item: '<li><a href="#"><h3 class="h3 type"></h3><p class="organisation"></p><p class="description"></p></a></li>',
+  plugins: [
+    // ListFuzzySearch()
+  ]
+}
 
-console.log(theList)
+var theList = new List('js-search', options, data)
 
+// Bricks.js
 const sizes = [
-  { columns: 1, gutter: 20 },                   // assumed to be mobile, because of the missing mq property
+  { columns: 1, gutter: 20 }, // assumed to be mobile, because of the missing mq property
   { mq: '360px', columns: 1, gutter: 20 },
   { mq: '480px', columns: 2, gutter: 20 },
   { mq: '600px', columns: 2, gutter: 20 },
   { mq: '800px', columns: 3, gutter: 20 }
 ]
 
-// create an instance
-
 const instance = Bricks({
   container: '.list',
-  packed:    'data-packed',        // if not prefixed with 'data-', it will be added
-  sizes:     sizes
+  packed: 'data-packed', // if not prefixed with 'data-', it will be added
+  sizes: sizes
 })
-
-instance
-  .on('pack',   () => console.log('ALL grid items packed.'))
-  .on('update', () => console.log('NEW grid items packed.'))
-  .on('resize', size => console.log('The grid has be re-packed to accommodate a new BREAKPOINT.'))
 
 // start it up, when the DOM is ready
 // note that if images are in the grid, you may need to wait for document.readyState === 'complete'
-
 document.addEventListener('DOMContentLoaded', event => {
   instance
-    .resize(true)     // bind resize handler
-    .pack()           // pack initial items
+    .resize(true) // bind resize handler
+    .pack() // pack initial items
 })
 
+// Triggers
 theList.on('sortStart', () =>
   instance.pack()
 )
 
 theList.on('searchComplete', () =>
-  //alert('searched')
+  // alert('searched')
   instance.pack()
 )
 
+// theList.fuzzySearch.search('craig')
+
+// Example bricks.js api
+/*
+instance
+  .on('pack', () => console.log('ALL grid items packed.'))
+  .on('update', () => console.log('NEW grid items packed.'))
+  .on('resize', size => console.log('The grid has be re-packed to accommodate a new BREAKPOINT.'))
+*/
+
+// Example filter code
 /*
 listObj.filter(function(item) {
    if (item.values().id > 1) {
@@ -93,11 +91,9 @@ listObj.filter(function(item) {
 listObj.filter(); // Remove all filters
 */
 
-// theList.fuzzySearch.search('craig')
-
+// Example need data
 /*
- Example need data
- {
+{
 "id": "56d9ba46a3b948fda0b49374",
 "description": "Interview & job seeking skills",
 "serviceProviderId": "lifeshare",
