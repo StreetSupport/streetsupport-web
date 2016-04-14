562fca7f30c18d3a8e5f487f15b3c1e16af1d2ae
Phil Lennon <phil@frontendphil.com>
2016-04-14 16:44:04 +0100

    add revised jasmine & linting tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,33 +12,25 @@ const tasks = requireDir(__dirname + '/tasks') // eslint-disable-line
 // Watch task
 gulp.task('watch', () => {
   gulp.watch(config.paths.scss + '**/*.scss', ['scss'])
-  gulp.watch(config.paths.js + '**/*.js', ['run-jasmine', 'webpack'])
+  gulp.watch(config.paths.spec + '**/*[Ss]pec.js', ['jasmine'])
+  gulp.watch(config.paths.js + '**/*.js', ['jasmine', 'jslint', 'webpack'])
   gulp.watch(config.paths.img + '{,**/}*.{png,jpg,gif,svg}', ['img'])
   gulp.watch(config.paths.icons + '**/*.svg', ['svgsprite'])
-  gulp.watch([config.paths.fonts + '**/*', config.paths.files + '**/*'] ['copy'])
-  gulp.watch(config.paths.specs + '**/*[Ss]pec.js', ['run-jasmine'])
+  gulp.watch([config.paths.fonts + '**/*', config.paths.files + '**/*'], ['copy'])
   gulp.watch([config.paths.layouts + '**/*.hbs', config.paths.pages + '**/*.hbs', config.paths.partials + '**/*.hbs'], ['metalsmith'])
 })
 
-// JS Dev Watch task
-gulp.task('dev-watch', () => {
-  gulp.watch(config.paths.specs + '**/*[Ss]pec.js', ['run-jasmine'])
-  gulp.watch(config.paths.js + '**/*.js', ['run-jasmine'])
-})
-
-// Run tests and watch js/spec files
-gulp.task('dev', callback => {
-  runSequence(
-    'run-jasmine',
-    'dev-watch',
-    callback
-  )
+// jsdev Watch task
+gulp.task('jsdevwatch', () => {
+  gulp.watch(config.paths.spec + '**/*[Ss]pec.js', ['jasmine', 'specsjslint'])
+  gulp.watch(config.paths.js + '**/*.js', ['jasmine'])
 })
 
 // Build website, either with development or minified assets and run server with live reloading
 gulp.task('default', callback => {
   runSequence(
-    'run-jasmine',
+    'jasmine',
+    'jslint',
     'clean',
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
@@ -50,7 +42,8 @@ gulp.task('default', callback => {
 // Build website, either with development or minified assets depending on flag
 gulp.task('deploy', callback => {
   runSequence(
-    'run-jasmine',
+    'jasmine',
+    'jslint',
     'clean',
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
@@ -59,7 +52,17 @@ gulp.task('deploy', callback => {
   )
 })
 
-// Run the audit task to check the code
+// Run tests and watch js/spec files
+gulp.task('jsdev', callback => {
+  runSequence(
+    'jasmine',
+    'specsjslint',
+    'jsdevwatch',
+    callback
+  )
+})
+
+// Run the audit task to check code standards
 gulp.task('auditcode', callback => {
   runSequence(
     'scsslint',
