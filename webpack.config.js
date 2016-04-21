d06a13b1059f2d9532cc0a05bc2b6818102a3265
vince (polyhat) <vslee888@gmail.com>
2016-04-21 17:16:34 +0100

    put some logic in 404 page to help users with old links

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     sponsor: path.join(__dirname, '/src/js/page-sponsor'),
     volunteer: path.join(__dirname, '/src/js/page-volunteer'),
     joinstreetsupport: path.join(__dirname, '/src/js/page-join-street-support'),
-    pageNotFound: path.join(__dirname, '/src/js/pageNotFound')
+    notFound: path.join(__dirname, '/src/js/page-not-found')
   },
   output: {
     path: path.join(__dirname, '/_dist/assets/js/'),
