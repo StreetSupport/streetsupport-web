9f1516e0db02dde5869cbe5921811d7c1f2e22ec
vince (polyhat) <vslee888@gmail.com>
2016-04-22 17:35:36 +0100

    just initialise social straight away

--- src/js/page-find-help.js
+++ src/js/page-find-help.js
@@ -15,25 +15,29 @@ var spin = document.getElementById('spin')
 var loading = new Spinner().spin(spin)
 
 // Get API data using promise
-getApiData.data(apiRoutes.serviceCategories).then(function (result) {
-  var data = result.data
-  forEach(data, function (category) {
-    if (category.key === 'meals' || category.key === 'dropin') {
-      category.page = 'category-by-day'
-    } else {
-      category.page = 'category'
+getApiData
+  .data(apiRoutes.serviceCategories)
+  .then(function (result) {
+    var data = result.data
+    forEach(data, function (category) {
+      if (category.key === 'meals' || category.key === 'dropin') {
+        category.page = 'category-by-day'
+      } else {
+        category.page = 'category'
+      }
+    })
+
+    var sorted = sortByOrder(data, ['sortOrder'], ['desc'])
+
+    // Append object name for Hogan
+    var theData = { categories: sorted }
+
+    var callback = function () {
+      loading.stop()
+      socialShare.init()
     }
+
+    templating.renderTemplate('js-category-list-tpl', theData, 'js-category-list-output', callback)
   })
 
-  var sorted = sortByOrder(data, ['sortOrder'], ['desc'])
-
-  // Append object name for Hogan
-  var theData = { categories: sorted }
-
-  var callback = function () {
-    loading.stop()
-    socialShare.init()
-  }
-
-  templating.renderTemplate('js-category-list-tpl', theData, 'js-category-list-output', callback)
-})
+socialShare.init()
--- src/js/social-share.js
+++ src/js/social-share.js
@@ -4,9 +4,15 @@ var el = '.social-share'
 
 var init = function () {
   // Twitter
+  console.log(twttr)
+  console.log('calling twttr.widgets.load')
+
   twttr.widgets.load(
     document.querySelectorAll(el)
   )
+
+  console.log(FB)
+  console.log('calling FB.init')
 
   // Facebook
   FB.init()
