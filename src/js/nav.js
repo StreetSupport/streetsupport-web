c7a6885614da3f7a6a6d944dc4ef084424487f52
Vince Lee <vslee888@gmail.com>
2016-09-30 14:56:13 +0100

    small sticky header

--- src/js/nav.js
+++ src/js/nav.js
@@ -56,6 +56,18 @@ var close = function () {
   }
 }
 
+window.addEventListener("scroll", (e) => {
+  let doc = document.documentElement
+  let top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
+  let header = document.querySelector('.header')
+  const smallStickyClass = 'header--small'
+  if (top >= 200) {
+    header.classList.add(smallStickyClass)
+  } else {
+    header.classList.remove(smallStickyClass)
+  }
+})
+
 module.exports = {
   init: init,
   open: open,
