e1a998696fefec872dd813e33d8b369c0f7f9cdc
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2016-10-10 12:38:52 +0100

    handle when user dismisses allow location dialog

--- src/js/location/get-location.js
+++ src/js/location/get-location.js
@@ -1,13 +1,17 @@
 var Q = require('q')
+var supportedCities = require('./supportedCities')
 
 var getLocation = function () {
   var deferred = Q.defer()
   var options = {
     maximumAge: 5 * 60 * 1000,
-    timeout: 5000
+    timeout: 3000
   }
 
+  let result = false
+
   function success (position) {
+    result = position
     deferred.resolve(position)
   }
 
@@ -16,6 +20,18 @@ var getLocation = function () {
   }
 
   navigator.geolocation.getCurrentPosition(success, error, options)
+
+  setTimeout(() => {
+    if (!result) {
+      let defaultCity = supportedCities.default()
+      deferred.resolve({
+        coords: {
+          latitude: defaultCity.latitude,
+          longitude: defaultCity.longitude
+        }
+      })
+    }
+  }, options.timeout + 1000)
 
   return deferred.promise
 }
