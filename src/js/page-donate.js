a0434b1fbac808973b4600176a264a37db004d1a
Vince Lee <vslee888@gmail.com>
2016-11-09 11:28:17 +0000

    fix encoding on donate orgs

--- src/js/page-donate.js
+++ src/js/page-donate.js
@@ -52,6 +52,7 @@ let getData = () => {
         })
         forEach(sorted, (p) => {
           p.description = htmlencode.htmlDecode(p.description)
+          p.providerName = htmlencode.htmlDecode(p.providerName)
         })
 
 
