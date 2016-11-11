7becdd728db282360adeb2393aec5d7609e0844f
Vince Lee <vslee888@gmail.com>
2016-11-11 13:47:36 +0000

    pull in stats

--- src/js/page-home.js
+++ src/js/page-home.js
@@ -3,29 +3,48 @@ let location = require('./location/locationSelector')
 const templating = require('./template-render')
 const browser = require('./browser')
 const socialShare = require('./social-share')
+const endpoints = require('./api')
+const api = require('./get-api-data')
+
+const init = (result) => {
+  let cityLabel = result.id === 'manchester'
+    ? 'Manchester'
+    : 'Leeds'
+
+  api
+    .data(endpoints.statistics + result.id + '/latest')
+    .then((stats) => {
+      console.log(stats)
+
+      let theData = {
+        isManchester: result.id === 'manchester',
+        isLeeds: result.id === 'leeds',
+        locations: location.getViewModel(result),
+        statistics: stats.data
+      }
+
+      console.log(theData)
+
+      var callback = function () {
+        location.handler(() => {
+          window.location.reload()
+        }, '.js-homepage-promo-location-selector')
+
+        document.querySelector('.js-city-label')
+          .innerHTML = 'in ' + cityLabel
+
+        browser.loaded()
+        socialShare.init()
+      }
+
+      templating.renderTemplate('js-content-tpl', theData, 'js-template-output', callback)
+    }, (_) => {
+      console.log(_)
+    })
+}
 
 location
   .getCurrent()
   .then((result) => {
-    let cityLabel = result.id === 'manchester'
-      ? 'Manchester'
-      : 'Leeds'
-    let theData = {
-      isManchester: result.id === 'manchester',
-      isLeeds: result.id === 'leeds',
-      locations: location.getViewModel(result)
-    }
-    var callback = function () {
-      location.handler(() => {
-        window.location.reload()
-      }, '.js-homepage-promo-location-selector')
-
-      document.querySelector('.js-city-label')
-        .innerHTML = 'in ' + cityLabel
-
-      browser.loaded()
-      socialShare.init()
-    }
-
-    templating.renderTemplate('js-content-tpl', theData, 'js-template-output', callback)
+    init(result)
   })
