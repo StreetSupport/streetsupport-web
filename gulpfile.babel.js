c49c2fa4023911906dc36739fc3482106a0870b2
Vince Lee <vslee888@gmail.com>
2016-11-24 15:22:25 +0000

    trying to run casper tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,6 @@ gulp.task('default', (callback) => {
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
     ['browsersync', 'watch'],
-    'casper',
     callback
   )
 })
@@ -49,6 +48,7 @@ gulp.task('deploy', (callback) => {
     'metalsmith',
     ['htmlmin', 'svgsprite', 'scss', 'webpack', 'img', 'copy'],
     'crticalcss',
+    'casper',
     callback
   )
 })
