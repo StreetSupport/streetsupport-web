587b305db185b062a053f97f1c5c561310dcfd0c
Vince Lee <vslee888@gmail.com>
2017-01-25 08:34:30 +0000

    parked

--- src/js/page-category-by-location.js
+++ src/js/page-category-by-location.js
@@ -3,11 +3,9 @@
 import './common'
 
 let FindHelp = require('./find-help')
-let apiRoutes = require('./api')
 
 let marked = require('marked')
 marked.setOptions({sanitize: true})
-let htmlEncode = require('htmlencode')
 
 let getApiData = require('./get-api-data')
 let querystring = require('./get-url-parameter')
@@ -19,25 +17,9 @@ let locationSelector = require('./location/locationSelector')
 let findHelp = null
 let currentLocation = null
 
-let onChangeLocation = (newLocation) => {
-  window.location.href = '/find-help/category?category=' + findHelp.theCategory + '&location=' + newLocation
-}
+import { buildFindHelpUrl, buildInfoWindowMarkup } from './pages/find-help/by-location/helpers'
 
-const buildFindHelpUrl = (locationResult) => {
-  const category = querystring.parameter('category')
-  const location = querystring.parameter('location')
-  const range = querystring.parameter('range')
-
-  let url = apiRoutes.cities + locationResult.findHelpId + '/services/' + findHelp.theCategory
-  if (location === 'my-location') {
-    url = apiRoutes.servicesByCategory + category + '/' + locationResult.latitude + '/' + locationResult.longitude
-  }
-  url += '?range=' + range
-
-  return url
-}
-
-const initMap = (providers, userLocation) => {
+const buildMap = (userLocation) => {
   const range = querystring.parameter('range')
   const zoomLevels = {
     '1000': 14,
@@ -46,40 +28,21 @@ const initMap = (providers, userLocation) => {
     '10000': 11
   }
   const centre = {lat: userLocation.latitude, lng: userLocation.longitude}
-  const map = new google.maps.Map(document.querySelector('.js-map'), {
+  return new google.maps.Map(document.querySelector('.js-map'), {
     zoom: zoomLevels[range],
     center: centre
   })
+}
+
+const initMap = (providers, userLocation) => {
+  const map = buildMap(userLocation)
 
   const infoWindows = []
 
   providers
     .forEach((p) => {
-      let previousDay = ''
-      const timeMarkup = p.openingTimes
-        .map((ot) => {
-          const titleClass = ot.day !== previousDay
-            ? ''
-            : 'hide-screen'
-          previousDay = ot.day
-          return `<dt class="map-info-window__opening-times-day ${titleClass}">${ot.day}</dt>
-            <dd class="map-info-window__opening-times-time">${ot.startTime} - ${ot.endTime}</dd>`
-        })
-        .join('')
-      const suitableForMarkup = p.tags.length > 0
-      ? `<p>Suitable for: ${p.tags.join(', ')}</p>`
-      : ''
-
       const infoWindow = new google.maps.InfoWindow({
-        content: `<div class="map-info-window">
-          <h1 class="h2"><a href="/find-help/organisation/?organisation=${p.serviceProviderId}">${p.serviceProviderName}</a></h1>
-          ${suitableForMarkup}
-          <p>${htmlEncode.htmlDecode(p.info)}</p>
-          <dl class="map-info-window__opening-times">${timeMarkup}</dl>
-          <a href="/find-help/organisation/?organisation=${p.serviceProviderId}" class="btn btn--brand-e">
-            <span class="btn__text">More about ${p.serviceProviderName}</span>
-          </a>
-        </div>`
+        content: buildInfoWindowMarkup(p)
       })
 
       infoWindows.push(infoWindow)
@@ -97,54 +60,62 @@ const initMap = (providers, userLocation) => {
     })
 }
 
+const getTemplate = (providers) => {
+  return providers.length > 0
+  ? 'js-category-result-tpl'
+  : 'js-category-no-results-result-tpl'
+}
+
+const onChangeLocation = (newLocation) => {
+  window.location.href = '/find-help/category?category=' + findHelp.theCategory + '&location=' + newLocation
+}
+
+const hasItemsCallback = (providers, locationResult) => {
+  locationSelector.handler(onChangeLocation)
+  findHelp.initFindHelpLocationSelector()
+  initMap(providers, locationResult)
+  browser.loaded()
+  socialShare.init()
+}
+
+const hasNoItemsCallback = () => {
+  locationSelector.handler(onChangeLocation)
+  findHelp.initFindHelpLocationSelector()
+  browser.loaded()
+  socialShare.init()
+}
+
+const getOnRenderCallback = (providers, locationResult) => {
+  return providers.length > 0
+  ? () => hasItemsCallback(providers, locationResult)
+  : () => hasNoItemsCallback()
+}
+
+const renderResults = (locationResult, result) => {
+  let theTitle = result.data.category.name + ' - Street Support'
+  document.title = theTitle
+
+  const template = getTemplate(result.data.providers)
+  const onRenderCallback = getOnRenderCallback(result.data.providers, locationResult)
+
+  analytics.init(theTitle)
+
+  let viewModel = {
+    categoryId: result.data.category.id,
+    categoryName: result.data.category.name,
+    categorySynopsis: marked(result.data.category.synopsis),
+    location: currentLocation.name
+  }
+  templating.renderTemplate(template, viewModel, 'js-category-result-output', onRenderCallback)
+}
+
 const buildList = (locationResult) => {
   getApiData.data(buildFindHelpUrl(locationResult))
   .then(function (result) {
     if (result.status === 'error') {
       window.location.replace('/find-help/')
     }
-    let theTitle = result.data.category.name + ' - Street Support'
-    document.title = theTitle
-
-    let template = ''
-    let onRenderCallback = function () {
-      locationSelector.handler(onChangeLocation)
-      findHelp.initFindHelpLocationSelector()
-      browser.loaded()
-      socialShare.init()
-    }
-
-    let formattedProviders = []
-    let subCategories = []
-
-    if (result.data.providers.length > 0) {
-      template = 'js-category-result-tpl'
-
-      onRenderCallback = () => {
-        locationSelector.handler(onChangeLocation)
-
-        findHelp.initFindHelpLocationSelector()
-
-        initMap(result.data.providers, locationResult)
-
-        browser.loaded()
-        socialShare.init()
-      }
-    } else {
-      template = 'js-category-no-results-result-tpl'
-    }
-
-    analytics.init(theTitle)
-
-    let viewModel = {
-      subCategories: subCategories,
-      shouldShowFilter: '' + formattedProviders.length > 1,
-      categoryId: result.data.category.id,
-      categoryName: result.data.category.name,
-      categorySynopsis: marked(result.data.category.synopsis),
-      location: currentLocation.name
-    }
-    templating.renderTemplate(template, viewModel, 'js-category-result-output', onRenderCallback)
+    renderResults(locationResult, result)
   })
 }
 
---
+++ src/js/pages/find-help/by-location/helpers.js
@@ -0,0 +1,44 @@
+const apiRoutes = require('../../../api')
+const querystring = require('../../../get-url-parameter')
+const htmlEncode = require('htmlencode')
+
+export const buildFindHelpUrl = (locationResult) => {
+  const category = querystring.parameter('category')
+  const location = querystring.parameter('location')
+  const range = querystring.parameter('range')
+
+  let url = apiRoutes.cities + locationResult.findHelpId + '/services/' + category
+  if (location === 'my-location') {
+    url = apiRoutes.servicesByCategory + category + '/' + locationResult.latitude + '/' + locationResult.longitude
+  }
+  url += '?range=' + range
+
+  return url
+}
+
+export const buildInfoWindowMarkup = (p) => {
+  let previousDay = ''
+  const timeMarkup = p.openingTimes
+    .map((ot) => {
+      const titleClass = ot.day !== previousDay
+        ? ''
+        : 'hide-screen'
+      previousDay = ot.day
+      return `<dt class="map-info-window__opening-times-day ${titleClass}">${ot.day}</dt>
+        <dd class="map-info-window__opening-times-time">${ot.startTime} - ${ot.endTime}</dd>`
+    })
+    .join('')
+  const suitableForMarkup = p.tags.length > 0
+  ? `<p>Suitable for: ${p.tags.join(', ')}</p>`
+  : ''
+
+  return `<div class="map-info-window">
+      <h1 class="h2"><a href="/find-help/organisation/?organisation=${p.serviceProviderId}">${p.serviceProviderName}</a></h1>
+      ${suitableForMarkup}
+      <p>${htmlEncode.htmlDecode(p.info)}</p>
+      <dl class="map-info-window__opening-times">${timeMarkup}</dl>
+      <a href="/find-help/organisation/?organisation=${p.serviceProviderId}" class="btn btn--brand-e">
+        <span class="btn__text">More about ${p.serviceProviderName}</span>
+      </a>
+    </div>`
+}
