31e05a3ac8fe3d4844c430369688542a5371f00d
Vince Lee <vslee888@gmail.com>
2017-01-31 17:16:16 +0000

    org map

--- src/js/page-all-service-providers.js
+++ src/js/page-all-service-providers.js
@@ -1,6 +1,7 @@
 // Common modules
 import './common'
 let sortBy = require('lodash/collection/sortBy')
+let htmlEncode = require('htmlEncode')
 
 let apiRoutes = require('./api')
 let getApiData = require('./get-api-data')
@@ -9,13 +10,87 @@ let browser = require('./browser')
 let querystring = require('./get-url-parameter')
 let locationSelector = require('./location/locationSelector')
 
-let currentLocation = null
-
 let onChangeLocation = (newLocation) => {
   window.location.href = '/find-help/all-service-providers/?location=' + newLocation
 }
 
-let getData = () => {
+const buildInfoWindowMarkup = (p) => {
+  return `<div class="map-info-window">
+      <h1 class="h2"><a href="/find-help/organisation/?organisation=${p.key}">${p.name}</a></h1>
+      <p>${htmlEncode.htmlDecode(p.shortDescription)}</p>
+      <a href="/find-help/organisation/?organisation=${p.key}" class="btn btn--brand-e">
+        <span class="btn__text">More about ${p.name}</span>
+      </a>
+    </div>`
+}
+
+const buildMap = (userLocation) => {
+  const centre = {lat: userLocation.latitude, lng: userLocation.longitude}
+  return new google.maps.Map(document.querySelector('.js-map'), {
+    zoom: 11,
+    center: centre
+  })
+}
+
+window.initMap = () => {}
+
+const displayMap = function(providers, userLocation) {
+  const map = buildMap(userLocation)
+
+  const infoWindows = []
+
+  providers
+    .forEach((p) => {
+      console.log(p)
+
+      if(p.addresses.length === 0) return
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: buildInfoWindowMarkup(p)
+      })
+
+      infoWindows.push(infoWindow)
+
+      console.log(infoWindow)
+
+      const marker = new google.maps.Marker({
+        position: { lat: p.addresses[0].latitude, lng: p.addresses[0].longitude },
+        map: map,
+        title: `${p.name}`
+      })
+
+      marker.addListener('click', () => {
+        infoWindows
+          .forEach((w) => w.close())
+        infoWindow.open(map, marker)
+      })
+    })
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        const myLocation = new google.maps.Marker({
+          position: pos,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 3,
+            fillColor: 'blue',
+            strokeColor: 'blue'
+          },
+          map: map
+        });
+      }, function() {
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
+}
+
+let getData = (currentLocation) => {
   if (window.location.search.length === 0) {
     var saved = document.cookie.replace(/(?:(?:^|.*;\s*)desired-location\s*=\s*([^;]*).*$)|^.*$/, '$1')
     if (saved !== undefined && saved.length > 0 && saved !== 'my-location') {
@@ -23,13 +98,12 @@ let getData = () => {
     }
   }
 
-  let location = querystring.parameter('location')
-
-  getApiData.data(apiRoutes.serviceProviders + location)
-    .then(function (result) {
-      let callback = function () {
+  getApiData.data(apiRoutes.serviceProviders + querystring.parameter('location'))
+    .then((result) => {
+      const callback = () => {
         locationSelector.handler(onChangeLocation)
         browser.loaded()
+        displayMap(result.data, currentLocation)
       }
       if (result.data.length === 0) {
         let theData = {
@@ -56,8 +130,7 @@ let init = () => {
   locationSelector
     .getCurrent()
     .then((result) => {
-      currentLocation = result
-      getData()
+      getData(result)
     })
 }
 
