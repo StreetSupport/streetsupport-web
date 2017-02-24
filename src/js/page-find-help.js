e0f137c5470e4f9a47076aac9ec03b128ca46435
Vince Lee <vslee888@gmail.com>
2017-02-24 15:00:13 +0000

    pull in find help data on build

--- src/js/page-find-help.js
+++ src/js/page-find-help.js
@@ -4,13 +4,10 @@ import 'babel-polyfill'
 
 // Page modules
 var socialShare = require('./social-share')
-var forEach = require('lodash/collection/forEach')
-var sortByOrder = require('lodash/collection/sortByOrder')
-var apiRoutes = require('./api')
-var getApiData = require('./get-api-data')
 var templating = require('./template-render')
 var browser = require('./browser')
 let locationSelector = require('./location/locationSelector')
+import { categories } from '../data/generated/service-categories'
 
 browser.loading()
 // Get API data using promise
@@ -18,42 +15,36 @@ browser.loading()
 let currentLocation = null
 
 let getData = () => {
-  getApiData
-    .data(apiRoutes.serviceCategories)
-    .then(function (result) {
-      var data = result.data
-      forEach(data, function (category) {
-        if (category.key === 'meals' || category.key === 'dropin') {
-          category.page = 'category-by-day'
-        } else {
-          category.page = 'category'
-        }
-      })
-
-      var sorted = sortByOrder(data, ['sortOrder'], ['desc'])
-
-      getApiData
-        .data(apiRoutes.cities)
-        .then((result) => {
-          const city = result.data.find((c) => c.id === currentLocation.id)
-
-          // Append object name for Hogan
-          var theData = {
-            categories: sorted,
-            location: city
-          }
-
-          var callback = function () {
-            document.querySelector('.js-city-label')
-              .innerHTML = 'in ' + currentLocation.name
-
-            browser.loaded()
-            socialShare.init()
-          }
-
-          templating.renderTemplate('js-category-list-tpl', theData, 'js-category-list-output', callback)
-        }, (_) => {})
+  categories
+    .forEach((category) => {
+      if (category.key === 'meals' || category.key === 'dropin') {
+        category.page = 'category-by-day'
+      } else {
+        category.page = 'category'
+      }
     })
+  var sorted = categories
+    .sort((a, b) => {
+      if (a.sortOrder < b.sortOrder) return -1
+      if (a.sortOrder > b.sortOrder) return 1
+      return 0
+    })
+
+  // Append object name for Hogan
+  var theData = {
+    categories: sorted,
+    location: currentLocation
+  }
+
+  var callback = function () {
+    document.querySelector('.js-city-label')
+      .innerHTML = 'in ' + currentLocation.name
+
+    browser.loaded()
+    socialShare.init()
+  }
+
+  templating.renderTemplate('js-category-list-tpl', theData, 'js-category-list-output', callback)
 }
 
 locationSelector
