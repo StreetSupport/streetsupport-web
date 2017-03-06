91abba041bb797fbfeeeabe8d93fbcecc9e0a173
Vince Lee <vslee888@gmail.com>
2017-03-06 15:39:37 +0000

    get supported cities during build and update registration schema

--- tasks/getLongTermData.js
+++ tasks/getLongTermData.js
@@ -10,7 +10,7 @@ import replace from 'gulp-replace'
 const endpoints = require('../src/js/api')
 
 /* calls API and generates static data */
-gulp.task('getLongTermData', (callback) => {
+gulp.task('service-categories', (callback) => {
   return request(endpoints.serviceCategories)
     .pipe(source(`${config.paths.generatedData}service-categories.js`))
     .pipe(streamify(jeditor(function (cats) {
@@ -25,3 +25,24 @@ gulp.task('getLongTermData', (callback) => {
     .pipe(replace('[', 'export const categories = ['))
     .pipe(gulp.dest('./'))
 })
+
+gulp.task('supported-cities', (callback) => {
+  return request(endpoints.cities)
+    .pipe(source(`${config.paths.generatedData}supported-cities.js`))
+    .pipe(streamify(jeditor(function (cities) {
+      return cities.map(function (c) {
+        return {
+          id: c.id,
+          name: c.name,
+          latitude: c.latitude,
+          longitude: c.longitude,
+          isOpenToRegistrations: c.isOpenToRegistrations,
+          isPublic: c.isPublic
+        }
+      })
+    })))
+    .pipe(replace('[', 'export const cities = ['))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('getLongTermData', ['service-categories', 'supported-cities'])
