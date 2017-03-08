ca8590313bab47a2ae47a8f54e775037177a3f8f
Vince Lee <vslee888@gmail.com>
2017-03-08 17:49:05 +0000

    elsewhere mobile nav

--- src/js/navigation/mobile-city-selection.js
+++ src/js/navigation/mobile-city-selection.js
@@ -1,11 +1,14 @@
 const location = require('../location/locationSelector')
+const supportedCities = require('../location/supportedCities')
 const browser = require('../browser')
 
 const handleClick = (e) => {
   e.preventDefault()
   let city = e.target.parentNode.getAttribute('data-city')
   location.setCurrent(city)
-  browser.redirect('/' + city)
+  if (city !== supportedCities.default().id) {
+    browser.redirect('/' + city)
+  }
 }
 
 const init = () => {
