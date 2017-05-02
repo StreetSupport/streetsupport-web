9876ed9a2b8b8f4fb6c6a69563e55b77a6c2458c
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2017-05-02 12:53:21 +0100

    uber basic accom details page

---
+++ spec/accommodation/details/testdata.js
@@ -0,0 +1,63 @@
+export const data = {
+  'links': {
+    'self': 'v1/accommodation/58f744dad021fb3db0e7f186'
+  },
+  'embedded': null,
+  'id': '58f744dad021fb3db0e7f186',
+  'temporaryAccommodationId': null,
+  'generalInfo': {
+    'name': 'added by temp accom admin',
+    'description': '',
+    'accommodationType': 'hosted',
+    'isOpenAccess': true,
+    'supportOffered': [
+      'alcohol',
+      'domestic violence',
+      'substances'
+    ],
+    'serviceProviderId': null,
+    'isPubliclyVisible': true
+  },
+  'contactInformation': {
+    'name': 'test',
+    'additionalInfo': '',
+    'email': 'test@test.com',
+    'telephone': null
+  },
+  'address': {
+    'associatedCityId': null,
+    'street1': 'tetes',
+    'street2': null,
+    'street3': null,
+    'city': 'manchester',
+    'postcode': 'm3 4bd',
+    'latitude': 53.4755361548836,
+    'longitude': -2.25848699844466,
+    'publicTransportInfo': '',
+    'nearestSupportProviderId': ''
+  },
+  'features': {
+    'acceptsHousingBenefit': false,
+    'acceptsNoHousingBenefitWithServiceProviderSupport': false,
+    'acceptsPets': 0,
+    'acceptsCouples': 0,
+    'hasDisabledAccess': true,
+    'isSuitableForWomen': true,
+    'isSuitableForYoungPeople': false,
+    'hasSingleRooms': false,
+    'hasSharedRooms': false,
+    'hasShowerBathroomFacilities': false,
+    'hasAccessToKitchen': false,
+    'hasFlexibleMealTimes': false,
+    'hasLounge': false,
+    'providesCleanBedding': false,
+    'allowsVisitors': false,
+    'hasOnSiteManager': false,
+    'referenceReferralIsRequired': false,
+    'price': 0,
+    'additionalFeatures': null,
+    'foodIsIncluded': false,
+    'availabilityOfMeals': null,
+    'featuresAvailableAtAdditionalCost': null
+  }
+}
--- src/js/models/accommodation/listing.js
+++ src/js/models/accommodation/listing.js
@@ -3,61 +3,11 @@ const endpoints = require('../../api')
 const browser = require('../../browser')
 const locationSelector = require('../../location/locationSelector')
 
+import { Accommodation, TypeFilter } from './types'
+
 const MapBuilder = require('./MapBuilder')
 
 const ko = require('knockout')
-
-const toUnique = (acc, name) => {
-  const match = acc.find((sc) => sc === name)
-  return match === undefined
-  ? [...acc, name]
-  : acc
-}
-
-const TypeFilter = function (name, listeners, isSelected = false) {
-  this.listeners = listeners
-  this.typeName = ko.observable(name)
-  this.isSelected = ko.observable(isSelected)
-  this.select = () => {
-    this.isSelected(true)
-    this.listeners.forEach((l) => l.typeFilterSelected(this))
-  }
-  this.deselect = () => {
-    this.isSelected(false)
-  }
-}
-
-const Accommodation = function (data, listeners) {
-  const self = this
-
-  const mapAddress = (data) => {
-    const parts = ['street1', 'street2', 'street3', 'city']
-    const result = parts
-      .map((p) => data[p])
-      .filter((p) => p && p !== undefined && p.length > 0)
-      .join(', ')
-    return `${result}. ${data.postcode}`
-  }
-
-  self.mapIndex = ko.observable(data.mapIndex)
-  self.mapIndexToDisplay = ko.observable(data.mapIndex + 1)
-  self.id = ko.observable(data.id)
-  self.name = ko.observable(data.name)
-  self.latitude = ko.observable(data.latitude)
-  self.longitude = ko.observable(data.longitude)
-  self.address = ko.observable(mapAddress(data))
-  self.additionalInfo = ko.observable(data.additionalInfo)
-  self.isOpenAccess = ko.observable(data.isOpenAccess)
-  self.accommodationType = ko.observable(data.accommodationType)
-  self.detailsUrl = ko.observable(`details?id=${data.id}`)
-  self.isActive = ko.observable()
-
-  self.selectItem = () => {
-    self.isActive(true)
-    listeners
-      .forEach((l) => l.itemSelected(self))
-  }
-}
 
 const AccommodationListing = function () {
   const self = this
@@ -125,9 +75,8 @@ const AccommodationListing = function () {
 
         self.items(result.data.items.map((i) => new Accommodation(i, [self.map, self])))
 
-        const types = result.data.items
-          .map((i) => i.accommodationType)
-          .reduce(toUnique, [])
+        const types = Array.from(new Set(result.data.items
+          .map((i) => i.accommodationType)))
           .map((i) => new TypeFilter(i, [self]))
         const all = new TypeFilter('all', [self], true)
         self.typeFilters([all, ...types])
