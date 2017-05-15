c903e44472a02b7d9e1304f420253fcb920ff7c6
Vince Lee <vslee888@gmail.com>
2017-05-15 17:34:11 +0100

    re-instated accom by map

---
+++ spec/accommodation/map/NoAccomSpec.js
@@ -0,0 +1,70 @@
+/* global describe, beforeEach, afterEach, it, expect */
+
+const ajaxGet = require('../../../src/js/get-api-data')
+const sinon = require('sinon')
+const Model = require('../../../src/js/models/accommodation/map')
+const browser = require('../../../src/js/browser')
+const querystring = require('../../../src/js/get-url-parameter')
+const locationSelector = require('../../../src/js/location/locationSelector')
+const gMaps = require('../../../src/js/models/accommodation/googleMaps')
+
+describe('Accommodation - Listing - No Accom', function () {
+  let sut = null
+
+  beforeEach(() => {
+    sinon.stub(ajaxGet, 'data')
+      .returns({
+        then: function (success, error) {
+          success({
+            'status': 'ok',
+            'statusCode': 200,
+            'data': noItemsReturned
+          })
+        }
+      })
+    sinon.stub(browser, 'loading')
+    sinon.stub(browser, 'loaded')
+    sinon.stub(browser, 'pushHistory')
+    sinon.stub(querystring, 'parameter')
+    sinon.stub(locationSelector, 'getCurrent')
+      .returns({
+        then: function (success, error) {
+          success({
+            latitude: 123.4,
+            longitude: 567.8
+          })
+        }
+      })
+    sinon.stub(gMaps, 'buildMap')
+    sinon.stub(gMaps, 'buildMarker')
+    sinon.stub(gMaps, 'buildInfoWindow')
+
+    sut = new Model()
+  })
+
+  afterEach(() => {
+    ajaxGet.data.restore()
+    browser.loading.restore()
+    browser.loaded.restore()
+    browser.pushHistory.restore()
+    querystring.parameter.restore()
+    locationSelector.getCurrent.restore()
+    gMaps.buildMap.restore()
+    gMaps.buildMarker.restore()
+    gMaps.buildInfoWindow.restore()
+  })
+
+  it('- should set noItemsAvailable to true', () => {
+    expect(sut.noItemsAvailable()).toBeTruthy()
+  })
+})
+
+const noItemsReturned = {
+  'links': {
+    'next': null,
+    'prev': null,
+    'self': '/v1/accommodation?index=0'
+  },
+  'items': [],
+  'total': 0
+}
