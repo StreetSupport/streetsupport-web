caf7c74e46a804b2af9b4ee984d05f04eff98089
Vince Lee <vslee888@gmail.com>
2017-09-28 14:02:09 +0100

    fix emergency help links

--- src/js/page-emergency-help.js
+++ src/js/page-emergency-help.js
@@ -17,6 +17,20 @@ getApiData
     const city = result.data.find((c) => c.id === locationid)
 
     const callback = () => {
+      if (browser.location().hash) {
+        browser.scrollTo(browser.location().hash)
+      }
     }
     templating.renderTemplate('js-swep-tpl', city, 'js-swep-output', callback)
   }, (_) => {})
+
+const links = document.querySelectorAll('a[href^="#"]')
+links
+  .forEach(l => {
+    l.addEventListener('click', (e) => {
+      e.preventDefault()
+      const anchor = l.hash.substring(1)
+      browser.scrollTo('#' + anchor)
+      browser.pushHistory({}, anchor, browser.location() + '#' + anchor)
+    })
+  })
