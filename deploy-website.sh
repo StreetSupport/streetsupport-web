8f5a6707c36500967bbd2046f3ead7421c99e296
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2017-12-27 18:56:04 +0000

    update uat/prod app service

--- deploy-website.sh
+++ deploy-website.sh
@@ -9,7 +9,7 @@ APIENVIRONMENT=1
 # Define variables depending on the branch
 if [[ $TRAVIS_BRANCH == 'release' ]]
   then
-    AZURE_WEBSITE=$PROD_AZURE_WEBSITE_W_EUR
+    AZURE_WEBSITE=$PROD_AZURE_WEBSITE
     APP_INSIGHTS_KEY=$PROD_APP_INSIGHTS_KEY
     APIENVIRONMENT=3
 fi
