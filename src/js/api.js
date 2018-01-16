f3544a5adc742311cfd1a854d2c844176d9bc9c6
Caroline Glassberg-Powell <caroline@glassberg-powell.com>
2018-01-16 20:42:09 +0000

    Use NODE_ENV instead of a custom env.js file
    It's the standard in the node community to use NODE_ENV as the
    environment variable. In addition, it's not recommended to have a file
    that can change that we don't want to change checked into the repo. If
    that number is inadvertently changed it could cause issues, although
    there may be safety checks around it that I'm unaware of.
    
    Therefore, remove the env.js file and replace it with NODE_ENV. This
    changed the deployment script, and the api.js file. Default to "1" as
    the CI environment.
    
    Add error checking in the api.js file to verify that the environment set
    is recognised.
    
    Also update the README to explain the environments a little bit more, as
    on first read not everyone may be familiar with "CI" and "UAT" as terms.
    And include "local" in the environment possibilities.

--- src/js/api.js
+++ src/js/api.js
@@ -3,11 +3,17 @@ var dev = 'https://ssn-api-dev.azurewebsites.net'
 var staging = 'https://ssn-api-uat.azurewebsites.net'
 var live = 'https://ssn-api-prod.azurewebsites.net'
 
-var env = require('./env')
+// Default the env to '1' for CI.
+// This can be overwritten by running gulp with NODE_ENV=X gulp.
+var env = process.env.NODE_ENV || 1
 
 var envs = [local, dev, staging, live]
 
 var domainRoot = envs[env]
+
+if (!domainRoot) {
+  throw new Error(`Environment ${env} not found`)
+}
 
 var p = function (url) {
   return domainRoot + url
