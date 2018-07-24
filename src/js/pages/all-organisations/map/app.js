2b7af2a37570d2f1f7189e7fc877463f9e595cb1
Vince Lee <vslee888@gmail.com>
2018-07-24 17:23:11 +0100

    tidy up

--- src/js/pages/all-organisations/map/app.js
+++ src/js/pages/all-organisations/map/app.js
@@ -16,71 +16,7 @@ const OrgRetrieval = require('../../../models/all-organisations/listing')
 const model = new OrgRetrieval()
 ko.applyBindings(model)
 
-let Popup
-
 window.initMap = function () {
-
-  Popup = function (position, content) {
-    this.position = position;
-    
-    var newDiv = document.createElement("div"); 
-    newDiv.innerHTML = content
-
-    this.anchor = document.createElement('div');
-    this.anchor.classList.add('card__gmaps-container');
-    this.anchor.appendChild(newDiv);
-
-    // Optionally stop clicks, etc., from bubbling up to the map.
-    this.stopEventPropagation();
-  };
-  // NOTE: google.maps.OverlayView is only defined once the Maps API has
-  // loaded. That is why Popup is defined inside initMap().
-  Popup.prototype = Object.create(google.maps.OverlayView.prototype);
-
-  /** Called when the popup is added to the map. */
-  Popup.prototype.onAdd = function () {
-    this.getPanes().floatPane.appendChild(this.anchor);
-  };
-
-  /** Called when the popup is removed from the map. */
-  Popup.prototype.onRemove = function () {
-    if (this.anchor.parentElement) {
-      this.anchor.parentElement.removeChild(this.anchor);
-    }
-  };
-
-  /** Called when the popup needs to draw itself. */
-  Popup.prototype.draw = function () {
-    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-    // Hide the popup when it is far out of view.
-    var display =
-      Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-        'block' :
-        'none';
-
-    if (display === 'block') {
-      this.anchor.style.left = divPosition.x + 'px';
-      this.anchor.style.top = divPosition.y + 'px';
-    }
-    if (this.anchor.style.display !== display) {
-      this.anchor.style.display = display;
-    }
-  };
-
-  /** Stops clicks/drags from bubbling up to the map. */
-  Popup.prototype.stopEventPropagation = function () {
-    var anchor = this.anchor;
-    anchor.style.cursor = 'auto';
-
-    ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
-      'pointerdown']
-      .forEach(function (event) {
-        anchor.addEventListener(event, function (e) {
-          e.stopPropagation();
-        });
-      });
-  };
-
   const buildInfoWindowMarkup = (p) => {
     return `<div class="card card--brand-h card--gmaps">
               <div class="card__title">
@@ -101,12 +37,10 @@ window.initMap = function () {
         p.locations
           .forEach((l) => {
             const marker = googleMaps.buildMarker(l, map, { title: `${htmlEncode.htmlDecode(p.name)}` })
-            marker.spKey = l.serviceProviderKey
-
-            google.maps.event.addListener(marker, 'click', function (...args) {
+            marker.addListener('click', function () {
               document.querySelectorAll('.card__gmaps-container')
                 .forEach((p) => p.parentNode.removeChild(p))
-              popup = new Popup(
+              popup = new googleMaps.Popup(
                 new google.maps.LatLng(this.position.lat(), this.position.lng()),
                 buildInfoWindowMarkup(p))
               popup.setMap(map)
@@ -124,5 +58,4 @@ window.initMap = function () {
         displayMap(newValue, result)
       })
   })
-
 }
