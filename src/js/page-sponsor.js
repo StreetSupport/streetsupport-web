d38ef7fd38ccceee739908b37393aa15f80b6f64
Vince Lee <vslee888@gmail.com>
2018-08-03 13:57:13 +0100

    updated partnership

--- src/js/page-sponsor.js
+++ src/js/page-sponsor.js
@@ -1,18 +1,5 @@
-// Common modules
 import './common'
 var ko = require('knockout')
 var Model = require('./models/SponsorModel')
-var location = require('./location/locationSelector')
-const templating = require('./template-render')
-const supportedCities = require('./location/supportedCities')
 
-let theData = {}
-supportedCities.locations
-  .forEach((c) => {
-    theData[`is${c.id}`] = location.getSelectedLocationId() === c.id
-  })
-let callback = () => {
-  ko.applyBindings(new Model())
-}
-
-templating.renderTemplate('js-content-tpl', theData, 'js-template-output', callback)
+ko.applyBindings(new Model())
