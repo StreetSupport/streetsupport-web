e53b90c074986fc13f83c20592e37af5d1667e84
Vince Lee <vslee888@gmail.com>
2018-08-07 13:32:02 +0100

    Revert "WIP: Fix/give help (#404)"
    This reverts commit dc8472c389fef2ef6b9d3a2754254195ef861b54.

--- src/js/pages/give-help/needs/app.js
+++ src/js/pages/give-help/needs/app.js
@@ -11,7 +11,6 @@ const templating = require('../../../template-render')
 import { formatNeeds } from '../../../models/give-help/requests/needs'
 import { buildList, initAutoComplete } from '../../../models/give-help/requests/listing'
 import { PostcodeProximity } from '../../../components/PostcodeProximity'
-import Hogan from 'hogan.js'
 
 const redirectForLegacyNeedDetails = () => {
   const cardId = getUrlParams.parameter('id').replace('/', '')
@@ -28,7 +27,7 @@ const defaultCallback = (currRange) => {
   browser.loaded()
 }
 
-const renderNeeds = (needs, userLocation, currRange, links) => {
+const renderNeeds = (needs, userLocation, currRange) => {
   const theData = {
     card: needs,
     location: userLocation.name,
@@ -50,78 +49,18 @@ const renderNeeds = (needs, userLocation, currRange, links) => {
   }
 }
 
-const renderShowMore = (isMore) => {console.log(isMore); templating.renderTemplate(
-  'js-show-more-tpl',
-  { isMore: isMore },
-  'js-show-more-btn-output',
-  () => {})}
-
-const initClickEvents = (data) => {
-  const clickEvents = [
-    {
-      selector: 'js-show-more-btn',
-      action: () => {
-        getApiData.data(apiRoutes.getFullUrl(data.links.next))
-          .then((result) => {
-            if (result.status === 'notFound') {
-              throw new Error({msg: 'page not found'})
-            }
-
-            // TODO: if (isError) {
-            //   removeError()
-            // }
-
-            const needs = formatNeeds(result.data.items, data.userLocation)
-            const theData = {
-              card: needs,
-              location: data.userLocation.name,
-              postcode: data.userLocation.postcode,
-              categoryName: 'requests for help',
-              geoLocationUnavailable: data.userLocation.geoLocationUnavailable
-            }
-            const links = result.data.links
-
-            var content = Hogan.compile(document.getElementById('js-cards-tpl').innerHTML)
-              .render(theData)
-            document.querySelector('#js-card-list').innerHTML +=
-              content
-
-            buildList()
-            redirectForLegacyNeedDetails()
-            listToSelect.init()
-            initAutoComplete(needs)
-
-            renderShowMore(links.next !== null)
-
-            const newData = Object.assign(Object.assign({}, data), {links: links})
-            initClickEvents(newData)
-          }).catch(() => {
-            // TODO: appendError()
-          })
-      }
-    }
-  ]
-  clickEvents
-    .forEach((e) => {
-      const elem = document.querySelector(`.${e.selector}`)
-      if (elem) elem.addEventListener('click', e.action)
-    })
-}
-
-const init = function (userLocation, range = 10000, pageSize = 21) {
+const init = function (userLocation, range = 10000) {
   if (userLocation) {
     const headerData = {
       postcode: userLocation.postcode,
       range: range
     }
     templating.renderTemplate('js-header-tpl', headerData, 'js-header-output', () => { })
-    const url = `${apiRoutes.needsHAL}?longitude=${userLocation.longitude}&latitude=${userLocation.latitude}&range=${range}&pageSize=${pageSize}`
+    const url = `${apiRoutes.needsHAL}?longitude=${userLocation.longitude}&latitude=${userLocation.latitude}&range=${range}&pageSize=100`
     getApiData.data(url)
       .then((result) => {
         const formatted = formatNeeds(result.data.items, userLocation)
         renderNeeds(formatted, userLocation, range)
-        renderShowMore(result.data.links.next !== null)
-        initClickEvents({links: result.data.links, userLocation: userLocation, currRange: range})
       }, () => {
         browser.redirect('/500')
       })
