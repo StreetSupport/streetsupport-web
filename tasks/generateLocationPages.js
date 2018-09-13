a0585c34b71dee678e173e17b615228aac70cf93
Jonathan Cowling <09cowlingj@gmail.com>
2018-09-13 22:13:45 +0100

    Fix options items not displaying as expected

--- tasks/generateLocationPages.js
+++ tasks/generateLocationPages.js
@@ -99,23 +99,31 @@ function loactionOptionTag(str, id, isSelected, name) {
   if (isSelected) {
     selectedStr = " selected=\"selected\""
   }
-  return str[0]+id+selectedStr+str[1]+name+str[2]
+  return str[0]+id+str[1]+selectedStr+str[2]+name+str[3]
 }
 
 gulp.task('l-generate-location-dropdown', () => {
   const srcFile = `${config.paths.partials}/locations/`
   const cityOutput = 
-  `<div class="form__fieldset-inline-fill-container">
-     <label class="hide-screen" for="location">Select Location</label>
-     <select name="location"
-       class="form__input form__input--inline-fill form__input--inline js-change-location-select">
-       <option value="">- Select a location -</option>${cities
-         .map((c) => loactionOptionTag`<option value="${c.id}">${c.name}</option>`)
-         .join(`\n`)}
-     </select>
-     <button class="btn btn--brand-e btn--inline-fill js-change-location-btn" type="submit">
-       <span class="btn__text">Visit location page</span>
-     </button>
+  `<div id="js-location-selector-output">
+     <form class="form location-selector js-change-location-form">
+       <fieldset class="form__fieldset form__fieldset--inline-fill">
+         <legend class="h2 hide-screen">Want to see what's available in our other featured locations?</legend>
+         <div class="form__fieldset-inline-fill-container">
+           <label class="hide-screen" for="location">Select Location</label>
+           <select name="location"
+             class="form__input form__input--inline-fill js-change-location-select"
+             id="js-location-selector-output">
+             <option value="">- Select a location -</option>${cities
+               .map((c) => loactionOptionTag`<option value="${c.id}"${c.selected}>${c.name}</option>`)
+               .join(`\n`)}
+           </select>
+           <button class="btn btn--brand-e btn--inline-fill js-change-location-btn" type="submit">
+             <span class="btn__text">Visit location page</span>
+           </button>
+         </div>
+       </fieldset>
+     </form>
    </div>`
 
   return newFile('_generated-selector-hogan.hbs', cityOutput).pipe(gulp.dest(srcFile))
