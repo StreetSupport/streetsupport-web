5a25c770298898fb73698ebf95ea623eedf1ca35
Vince Lee <vslee888@gmail.com>
2018-09-20 12:18:42 +0100

    added southampton charter banner

--- src/js/pages/locations/app.js
+++ src/js/pages/locations/app.js
@@ -165,6 +165,11 @@ initStatistics(currentLocation)
 initMap(currentLocation)
 initSwep(currentLocation.id)
 
+if(currentLocation.id === 'southampton') {
+  console.log('soton')
+  templating.renderTemplate('js-soton-charter-tpl', {}, 'js-soton-charter-output')
+}
+
 const availableFeatures = [
   { isEnabledFlag: 'toolkitIsEnabled', init: initToolkit },
   { isEnabledFlag: 'charterIsEnabled', init: initCharter },
