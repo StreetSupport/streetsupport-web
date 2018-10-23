31a2ab8c12023b72083aaa6845c555464bc15509
Paul Aldred-Bann <pcaldredbann@gmail.com>
2018-10-23 12:27:08 +0100

    added functionality to help finder to allow partial postcodes (outcodes) on search (#432)

--- src/js/location/postcodes.js
+++ src/js/location/postcodes.js
@@ -33,7 +33,24 @@ export const getCoords = (postcode, success, failure) => {
   if (cachedPostcode) {
     success(cachedPostcode)
   } else {
-    ajaxGet
+    if (isOutcode(postcode)) {
+      ajaxGet
+        .data(`https://api.postcodes.io/outcodes/${postcode}`)
+        .then((postcodeResult) => {
+          if (postcodeResult.status === 'ok') {
+            const result = {
+              postcode: postcodeResult.data.result.postcode,
+              longitude: postcodeResult.data.result.longitude,
+              latitude: postcodeResult.data.result.latitude
+            }
+            storage.set(clean(postcode), result)
+            success(result)
+          } else {
+            failure()
+          }
+        }, failure)
+    } else {
+      ajaxGet
       .data(`https://api.postcodes.io/postcodes/${postcode}`)
       .then((postcodeResult) => {
         if (postcodeResult.status === 'ok') {
@@ -48,5 +65,10 @@ export const getCoords = (postcode, success, failure) => {
           failure()
         }
       }, failure)
+    }
   }
+}
+
+const isOutcode = (postcode) => {
+  return postcode && postcode.replace(/\s/g, '').length < 5;
 }
