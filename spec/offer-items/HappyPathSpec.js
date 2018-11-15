073291c7035595151ade6bb10da3db051a1ba048
Vince Lee <vslee888@gmail.com>
2018-11-15 10:38:34 +0000

    get offer categories in build

--- spec/offer-items/HappyPathSpec.js
+++ spec/offer-items/HappyPathSpec.js
@@ -1,52 +1,41 @@
-/* global describe, beforeEach, afterEach, it, expect */
+/* global describe, beforeAll, afterAll, it, expect */
 
-var getApi = require('../../src/js/get-api-data')
-var postToApi = require('../../src/js/post-api-data')
-var sinon = require('sinon')
-var Model = require('../../src/js/models/give-help/offer-items/OfferItemsModel')
-var endpoints = require('../../src/js/api')
-var browser = require('../../src/js/browser')
+const browser = require('../../src/js/browser')
+const endpoints = require('../../src/js/api')
+const Model = require('../../src/js/models/give-help/offer-items/OfferItemsModel')
+const postToApi = require('../../src/js/post-api-data')
+const sinon = require('sinon')
+import { categories } from '../../src/data/generated/need-categories'
 
 describe('Offer Items', () => {
   let model = null
   let browserLoadingStub = null
   let browserLoadedStub = null
 
-  beforeEach(() => {
+  beforeAll(() => {
     browserLoadingStub = sinon.stub(browser, 'loading')
     browserLoadedStub = sinon.stub(browser, 'loaded')
     sinon.stub(browser, 'jumpTo')
     sinon.stub(browser, 'scrollTo')
-    sinon.stub(getApi, 'data')
-      .returns({
-        then: function (success, error) {
-          success({
-            'status': 'ok',
-            'statusCode': 200,
-            'data': catData
-          })
-        }
-      })
 
     model = new Model('manchester')
   })
 
-  afterEach(() => {
+  afterAll(() => {
     browser.loading.restore()
     browser.loaded.restore()
     browser.jumpTo.restore()
     browser.scrollTo.restore()
-    getApi.data.restore()
   })
 
   it('- Should populate categories', () => {
-    expect(model.categories().length).toEqual(10)
+    expect(model.categories().length).toEqual(categories.length)
   })
 
   describe('- Post Offer', () => {
     let postStub = null
 
-    beforeEach(() => {
+    beforeAll(() => {
       postStub = sinon
         .stub(postToApi, 'post')
         .returns({
@@ -71,7 +60,7 @@ describe('Offer Items', () => {
       model.submitForm()
     })
 
-    afterEach(() => {
+    afterAll(() => {
       postToApi.post.restore()
     })
 
@@ -91,7 +80,7 @@ describe('Offer Items', () => {
           'Postcode': 'postcode',
           'Description': 'description',
           'AdditionalInfo': '',
-          'SelectedCategories': [catData[0].key],
+          'SelectedCategories': [categories[0].key],
           'IsOptedIn': false,
           'OtherCategory': ''
         }).calledAfter(browserLoadingStub)).toBeTruthy()
@@ -110,46 +99,3 @@ describe('Offer Items', () => {
     })
   })
 })
-
-const catData = [
-  {
-    'key': 'food-and-drink',
-    'value': 'Food and Drink'
-  },
-  {
-    'key': 'toiletries',
-    'value': 'Toiletries'
-  },
-  {
-    'key': 'clothes',
-    'value': 'Clothes'
-  },
-  {
-    'key': 'materials-for-activities',
-    'value': 'Materials for Activities'
-  },
-  {
-    'key': 'services',
-    'value': 'Services (printing etc)'
-  },
-  {
-    'key': 'furniture',
-    'value': 'Furniture'
-  },
-  {
-    'key': 'bedding',
-    'value': 'Bedding'
-  },
-  {
-    'key': 'cleaning-materials',
-    'value': 'Cleaning Materials'
-  },
-  {
-    'key': 'kitchenware',
-    'value': 'Kitchenware'
-  },
-  {
-    'key': 'electrical-items',
-    'value': 'Electrical Items'
-  }
-]
