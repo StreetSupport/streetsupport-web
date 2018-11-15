83be064aa86970b76a026f4df8e44e32c907ee3b
Vince Lee <vslee888@gmail.com>
2018-11-15 17:05:36 +0000

    bcp change for good on /give-help/

--- src/js/models/give-help/local-campaigns.js
+++ src/js/models/give-help/local-campaigns.js
@@ -20,8 +20,9 @@ export default class LocalCampaigns {
       this.locationName = location.name
       this.realChangeUrl = location.realChangeUrl
       this.realChangeTitle = location.realChangeTitle
-      this.isWiganRealChangeTemplate = location.key === "wigan-and-leigh"
-      this.isDefaultTemplate = location.key === "leeds"
+      this.isWiganRealChangeTemplate = location.key === 'wigan-and-leigh'
+      this.isBournemouthTemplate = location.key === 'bournemouth'
+      this.isDefaultTemplate = location.key === 'leeds'
 
       campaigns
         .forEach((c) => {
