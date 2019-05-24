9308db4a10d553ede976ac5d33df8d293658d83d
Vince Lee <vslee888@gmail.com>
2019-05-24 13:46:28 +0100

    tim b's browserstack integration

---
+++ automated-tests/homepage/homepage.spec.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+describe('Homepage behaviour', function() {
+
+  beforeAll(() => {
+    browser
+      .url('/')
+      .pause(5000)
+  });
+
+  it('has title', function () {    
+    expect(browser.getTitle()).toBe("Street Support Network - Working together to end homelessness");
+  });
+
+  it('has Find Help CTA', function () {    
+    var findHelpCTA = browser.element('.btn--brand-d=Find Help');
+    expect(findHelpCTA).not.toBeNull();
+  });
+
+  it('has Give Help CTA', function () {    
+    var giveHelpCTA = browser.element('.btn--brand-e=Give Help');
+    expect(giveHelpCTA).not.toBeNull();
+  });
+});
