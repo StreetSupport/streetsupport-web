af1c5492d1b8839e634c2d8bd3b31f21cb05796c
oleksandr seliuchenko <o.seliuchenko@honeycombsoft.com>
2020-10-01 18:43:54 +0300

    Add new needs pages

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
 var local = 'http://localhost:55881'
-var dev = 'https://ssn-api-dev.azurewebsites.net'
+var dev = 'http://localhost:55881' // 'https://ssn-api-dev.azurewebsites.net'
 var staging = 'https://ssn-api-uat.azurewebsites.net'
 var live = 'https://ssn-api-prod.azurewebsites.net'
 
---
+++ src/js/pages/give-help/by-client-group/needs/app.js
@@ -0,0 +1,8 @@
+// Common modules
+import '../../../../common'
+
+// Page modules
+var ko = require('knockout')
+var Model = require('../../../../models/give-help/by-client-group/requests/listing')
+
+ko.applyBindings(new Model())
