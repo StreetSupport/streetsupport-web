e2398cc95af2430358b702f5aea374d04b3c091f
oleksandr seliuchenko <o.seliuchenko@honeycombsoft.com>
2020-11-05 18:56:08 +0200

    Add header

--- src/js/models/families/families.js
+++ src/js/models/families/families.js
@@ -1,8 +1,13 @@
+const browser = require('../../browser')
 const SearchFamilyAdviceModule = require('../../pages/families/search-family-advice/search-family-advice')
 
 function Families () {
   const self = this
   self.searchFamilyAdvice = new SearchFamilyAdviceModule.SearchFamilyAdvice()
+
+  self.redirectToSupport = function () {
+    browser.redirect('https://streetsupport.net/')
+  } 
 }
 
 module.exports = Families
