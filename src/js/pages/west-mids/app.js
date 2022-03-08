ed26e16b8628cca441e3bfd59bc8791793d1f09c
o-seliuchenko <69303692+o-seliuchenko@users.noreply.github.com>
2022-03-08 14:20:05 +0200

    Fix finf help (#662)

--- src/js/pages/west-mids/app.js
+++ src/js/pages/west-mids/app.js
@@ -1,36 +1,52 @@
 // Common modules
 import '../../common'
 import { cities } from '../../../data/generated/supported-cities'
+import { categories } from '../../../data/generated/service-categories'
 import htmlEncode from 'htmlencode'
 
 const api = require('../../get-api-data')
 const browser = require('../../browser')
 const endpoints = require('../../api')
-const location = require('../../location/locationSelector')
 const MapBuilder = require('../../models/accommodation/MapBuilder')
 const templating = require('../../template-render')
 const wp = require('../../wordpress')
+const supportedCities = require('../../../js/location/supportedCities')
+const mainCity = 'birmingham'
+const countyKey = 'west-midlands'
 
-const initLocations = function (currentLocation) {
+
+const initFindHelp = function (currentLocation) {
+  const cats = categories
+  cats.find((c) => c.key === 'accom').key = 'accommodation'
+
   const ui = {
-    form: document.querySelector('.js-change-location-form'),
-    select: document.querySelector('.js-change-location-select')
+    form: document.querySelector('.js-find-help-form'),
+    select: document.querySelector('.js-find-help-cats'),
+    postcode: document.querySelector('.js-find-help-postcode')
   }
+
+  const allOrganisationsOption = document.createElement('option')
+  allOrganisationsOption.setAttribute('value', 'all-service-providers')
+  allOrganisationsOption.innerText = 'All Organisations'
+  ui.select.appendChild(allOrganisationsOption)
+
+  cats
+    .forEach((c) => {
+      const option = document.createElement('option')
+      option.setAttribute('value', c.key)
+      option.innerText = c.name
+      ui.select.appendChild(option)
+    })
+
+  ui.postcode.setAttribute('placeholder', `your postcode: eg ${currentLocation.postcode}`)
+  ui.postcode.value = currentLocation.postcode
+
   ui.form.addEventListener('submit', function (e) {
     e.preventDefault()
-    const reqLocation = ui.select.value
-    if (reqLocation) {
-      location.setCurrent(reqLocation)
-      browser.redirect(`/${reqLocation}/advice`)
-    }
+    const reqPostcode = ui.postcode.value
+    const reqService = ui.select.value
+    browser.redirect(`/find-help/${reqService}/?postcode=${reqPostcode}`)
   })
-
-  if (currentLocation) {
-    Array.from(document.querySelector('.js-change-location-select'))
-      .filter((t) => t.tagName === 'OPTION')
-      .find((o) => o.value === currentLocation.id)
-      .setAttribute('selected', 'selected')
-  }
 }
 
 window.initMap = () => { }
@@ -66,11 +82,11 @@ const initMap = function (currentLocation) {
 const initNews = function () {
   const totalPostsToShow = 3
   wp
-    .getPostsByTag('west-midlands', totalPostsToShow, 0, true)
+    .getPostsByTag(countyKey, totalPostsToShow, 0, true)
     .then((result) => {
       if (result.posts.length === totalPostsToShow) {
         result.taxonomy.name = 'West Midlands'
-        result.taxonomy.link = 'https://news.streetsupport.net/tag/west-midlands/'
+        result.taxonomy.link = `https://news.streetsupport.net/tag/${countyKey}/`
         templating.renderTemplate('js-news-tpl', result, 'js-news-output')
       }
     })
@@ -86,7 +102,7 @@ const initStatistics = function () {
   const requiredStats = ['totalServiceProviders', 'totalNeeds', 'totalServices']
 
   api
-    .data(endpoints.statistics + 'west-midlands' + '/latest')
+    .data(endpoints.statistics + countyKey + '/latest')
     .then((result) => {
       const theData = {
         statistics: requiredStats
@@ -105,12 +121,10 @@ const initStatistics = function () {
     })
 }
 
+
+const currentLocation = supportedCities.get(mainCity)
+
+initFindHelp(currentLocation)
 initNews()
 initStatistics()
-initMap(cities.find((c) => c.id === 'birmingham'))
-
-location
-  .getPreviouslySetPostcode()
-  .then((result) => {
-    initLocations(result)
-  })
+initMap(cities.find((c) => c.id === mainCity))
