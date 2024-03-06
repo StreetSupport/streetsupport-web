102fa72bd927f1e2156565450ac23cd96ca890ac
o.seliuchenko <o.seliuchenko@honeycombsoft.com>
2024-03-06 23:40:43 +0200

    Update all packages

--- tasks/fileHelpers.js
+++ tasks/fileHelpers.js
@@ -3,7 +3,7 @@ import util from 'gulp-util'
 export function newFile (name, contents) {
   var readableStream = require('stream').Readable({ objectMode: true })
   readableStream._read = function () {
-    this.push(new util.File({ cwd: '', base: '', path: name, contents: new Buffer(contents) }))
+    this.push(new util.File({ cwd: '', base: '', path: name, contents: new Buffer.from(contents) }))
     this.push(null)
   }
   return readableStream
