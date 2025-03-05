d55b3466885383daea96739e60ce6e5e00ccd47f
JAMES-PC\jimmy <jimmy.cross@gmail.com>
2025-03-05 08:21:34 +0000

    refactor: comment out initFeaturedNews function and related template in reading page

--- src/js/pages/locations/app.js
+++ src/js/pages/locations/app.js
@@ -54,18 +54,18 @@ const initNews = function (currentLocationId) {
     })
 }
 
-const initFeaturedNews = function () {
-  const totalPostsToShow = 1
-  wp
-    .getPostsByTags(['reading', 'featured'], totalPostsToShow, 0, true)
-    .then((result) => {
-      if (result.posts.length === totalPostsToShow) {
-        result.taxonomy.name = 'volunteer-for-good and featured'
-        result.taxonomy.link = 'https://news.streetsupport.net/tag/volunteer-for-good/'
-        templating.renderTemplate('js-news-tpl', result, 'js-news-output')
-      }
-    })
-}
+// const initFeaturedNews = function (currentLocation) {
+//   const totalPostsToShow = 1
+//   wp
+//     .getPostsByTags([currentLocation.id, 'featured'], totalPostsToShow, 0, true)
+//     .then((result) => {
+//       if (result.posts.length === totalPostsToShow) {
+//         result.taxonomy.name = `Featured in ${currentLocation.id}`
+//         result.taxonomy.link = `https://news.streetsupport.net/tag/${currentLocation.id}/`
+//         templating.renderTemplate('js-news-tpl', result, 'js-news-output')
+//       }
+//     })
+// }
 
 const initFindHelp = function (currentLocation) {
   const cats = categories
@@ -179,7 +179,7 @@ const currentLocation = location.getCurrentHub()
 
 initLocations(currentLocation.id)
 initNews(currentLocation.id)
-initFeaturedNews(currentLocation.id)
+// initFeaturedNews(currentLocation)
 initFindHelp(currentLocation)
 initStatistics(currentLocation)
 initMap(currentLocation)
