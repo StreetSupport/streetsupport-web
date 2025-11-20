2c227cd03bf1367d1a2d242042cb38cf36dea2c7
James Cross <james@jmcross.uk>
2025-11-20 09:00:59 +0000

    feature: add inactive SWEP notification banner to all severe weather accommodation pages

--- src/js/page-swep.js
+++ src/js/page-swep.js
@@ -3,16 +3,55 @@ import './common'
 const locationSelector = require('./location/locationSelector')
 const getApiData = require('./get-api-data')
 const apiRoutes = require('./api')
-const templating = require('./template-render')
+const browser = require('./browser')
 
-const locationId = locationSelector.getCurrentHub()
+const location = locationSelector.getCurrentHub()
+const locationId = location.id
+
+const showInactiveNotice = function (locationName) {
+  const container = document.getElementById('js-swep-availability-status')
+  if (!container) return
+
+  container.innerHTML = `
+    <div class="block block--swep-inactive-notice">
+      <div class="container">
+        <div class="block__content block__content--swep-notice">
+          <h2 class="h2">Severe Weather Emergency Accommodation is not currently active in ${locationName}</h2>
+          <p>This page provides information about emergency accommodation during severe weather. The service is activated when temperatures drop below freezing.</p>
+          <p>If you need help right now, please visit:</p>
+          <div class="swep-notice-ctas">
+            <a href="/${locationId}/advice/" class="btn btn--brand-g">
+              <span class="btn__text">See emergency advice</span>
+            </a>
+            <a href="/find-help/" class="btn btn--brand-e">
+              <span class="btn__text">Find Help</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+const hidePageContent = function () {
+  const mainContent = document.querySelectorAll('.js-swep-content')
+  mainContent.forEach((element) => {
+    element.style.opacity = '0.6'
+  })
+}
 
 getApiData
   .data(apiRoutes.cities)
   .then((result) => {
     const city = result.data.find((c) => c.id === locationId)
 
-    const callback = () => {
+    if (city && !city.swepIsAvailable) {
+      const locationName = city.name || locationId
+      showInactiveNotice(locationName)
+      hidePageContent()
     }
-    templating.renderTemplate('js-swep-tpl', city, 'js-swep-output', callback)
-  }, (_) => {})
+
+    browser.loaded()
+  }, (_) => {
+    browser.loaded()
+  })
